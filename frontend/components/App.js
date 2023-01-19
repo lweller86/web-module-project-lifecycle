0fbb9c98a0dae7c13502be3bffed5b17f38b8d83
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-01-19 12:06:19 -0500

    Todo list fully Functional as a single page App

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,45 +5,72 @@ const URL = 'http://localhost:9000/api/todos'
 
 
 export default class App extends React.Component {
-  state={
-    todos:[],
-    error:'',
-    name:'',
+  state = {
+    todos: [],
+    error: '',
+    name: '',
+    displayCompleteds: false
   }
+
+  setAxiosResponseError = err => this.setState({ ...this.state, error: err.respnse.data.message })
+
+  resetForm = () => this.setState({ ...this.state, name: '' })
+
+
 
   onNameChange = evt => {
     const { value } = evt.target
-    this.setState({ ...this.state, name: value})
+    this.setState({ ...this.state, name: value })
   }
 
-fetchAllTodos= () => {
-  axios.get(URL)
-  .then(res => {
-    this.setState({... this.state, todos: res.data.data})
-  })
-  .catch(err => {
-    this.setState({ ...this.state, error: err.response.data.message})
-  })
-}
 
-postNewTodo = () => {
-  axios.post(URL, {name: this.state.name})
-  .then(res=> {
-    this.fetchAllTodos()
-    this.setState({...this.state, name:''})
-  })
-  .catch(err => {
-    debugger
-  })
-}
-onSubmit = evt => {
-  evt.preventDefault()
-  this.postNewTodo()
 
-}
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({ ... this.state, todos: res.data.data })
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
+
+
+  postNewTodo = () => {
+    axios.post(URL, { name: this.state.name })
+      .then(res => {
+        this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
+        this.resetForm()
+
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
+  toggleComplete = id => evt => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        this.setState({
+          ...this.state, todos: this.state.todos.map(td => {
+            if (td.id !== id) return td
+            return res.data.data
+          })
+        })
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
+  onSubmit = evt => {
+    evt.preventDefault()
+    this.postNewTodo()
+
+  }
+
+  removeCompleted = () => {
+    this.setState({ ...this.state, displayCompleteds: !this.state.displayComplteds })
+  }
+
 
   componentDidMount() {
-this.fetchAllTodos()
+    this.fetchAllTodos()
   }
   render() {
     return (
@@ -51,15 +78,22 @@ this.fetchAllTodos()
         <div id="errors">Error:{this.state.error} </div>
         <div id="todos">
           <h2>Todos:</h2>
-          {this.state.todos.map(td => {
-            return <div key={td.id}>{td.name} </div>
-          })}
+          {this.state.todos.reduce((acc, td) => {
+            if (this.state.displayCompleteds || !td.completed ) return acc.concat(
+              <div key={td.id} onClick={this.toggleComplete(td.id)}>{td.name} {td.completed ? ' ✔️' : ''} </div>
+              )
+            else return acc
+          }, [])
+
+           // 
+          }
         </div>
         <form id="todoForm" onSubmit={this.onSubmit}>
-        <input value={this.state.name} onChange={this.onNameChange} type="text" placeholder="Input Todo"></input>
-        <input type="submit"></input>
-        <button>Clear Completed</button>     
-      </form>
+          <input value={this.state.name} onChange={this.onNameChange} type="text" placeholder="Input Todo"></input>
+          <input type="submit"></input>
+
+        </form>
+        <button onClick={this.removeCompleted}>{this.state.displayCompleteds ? 'Hide' : 'Show'}Clear Completed</button>
       </div>
     )
   }
